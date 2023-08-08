dc038639038163dddde362a274276e36a432e8ab
Hashimjasem <j_hashim1305@outlook.com>
2023-08-08 14:36:42 +0800

    Layout added

---
+++ curbmarket/src/pages/Home.js
@@ -0,0 +1,16 @@
+function Home() {
+    return ( 
+        <div>
+            {/* search bar, filter
+                --> feed*/}
+
+            {/* popular catagories 
+                --> feed */}
+                
+            {/* for you 
+                --> feed*/}
+        </div>
+     );
+}
+
+export default Home;
